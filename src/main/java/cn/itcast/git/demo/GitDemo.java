b2d25089e6792c4a7ac74c9807e4d3b8019923a6
yang-kai-01 <62297969+yang-kai-01@users.noreply.github.com>
2020-03-18 00:08:18 +0800

    Update GitDemo.java

--- src/main/java/cn/itcast/git/demo/GitDemo.java
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -2,6 +2,6 @@ package cn.itcast.git.demo;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("撒浪嘿呦");
+        System.out.println("你好啊，git");
     }
 }
